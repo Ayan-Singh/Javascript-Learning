aa3fdd34c073c8b16d319c3f50fea89557c617c6
Ayan Singh <ayansingh2042@gmail.com>
2024-10-09 21:53:13 +0530

    commit1

---
+++ index.js
@@ -0,0 +1,18 @@
+//Cheking if number is greater than other number 
+ let num1 = 23;
+ let num2 = 32;
+ if(num1>num2){
+    console.log("Num 1 is greater")
+ }else{
+    console.log("Num 2 is greater")
+ }
+
+
+ //check if string is equal 
+ let user1 = "child";
+ let user2="child";
+ if(user1==user2){
+    console.log("matched");
+ }else{
+    console.log("no match")
+ }
