6d64a74791a8318a1d2bb2fb84f56961baa32de9
Ayan Singh <ayansingh2042@gmail.com>
2024-10-13 19:06:54 +0530

    3rd commit

--- index2.js
+++ index2.js
@@ -8,3 +8,14 @@ for(let i=0;i<=names.length-1;i++){
    }
 }
 console.log(naam)
+
+let names1 = ["ayan","gaurav","poorvi","pooru"]
+let naam2 =[];
+for(let i=0;i<=names.length-1;i++){
+   if(names[i]==="poorvi"){
+      continue
+   }else{
+      naam.push(names[i]) ;
+   }
+}
+console.log(naam)
