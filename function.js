ca37013022f37a29137bd5cff5a39ced64656db2
Ayan Singh <ayansingh2042@gmail.com>
2024-10-14 20:20:20 +0530

    5th commit

--- function.js
+++ function.js
@@ -7,3 +7,20 @@ function makeTea(typeofTea){
     return `Making ${typeofTea} `
 }
 console.log(makeTea("green tea"))
+
+//function inside function
+
+function orderTea(type){
+    function orderConfirm(){
+        return `Order for ${type} is confirmed`
+    }
+    return orderConfirm()
+}
+console.log(orderTea("chai"))
+
+//arrow function
+
+const calculateTotal = (price , quantity) => {let total = price*quantity 
+    return total
+};
+console.log(calculateTotal(4,8))
