ab577dd473c76e77341d466912e15620b119ac2e
Ayan Singh <ayansingh2042@gmail.com>
2024-10-14 19:47:45 +0530

    4th commit

---
+++ function.js
@@ -0,0 +1,9 @@
+function greet(name){
+    return `Hello ${name} , How are you today!`
+}
+console.log(greet("Ayaaan"))
+
+function makeTea(typeofTea){
+    return `Making ${typeofTea} `
+}
+console.log(makeTea("green tea"))
